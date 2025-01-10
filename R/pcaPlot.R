5ee904f9cae2b5547c170358f6cc2dc0c63036c6
cr403 <151646120+cr403@users.noreply.github.com>
2025-01-10 12:54:45 -0500

    Update pcaPlot.R
    Changes from Ben's PCA function: 
    * Added optional ellipses into the function call 
    * Added flexibility of defining principle components of interest
    * Added `customColors` option for defining color pallettes

--- R/pcaPlot.R
+++ R/pcaPlot.R
@@ -1,88 +1,97 @@
 --------------------
 PCA plot and biplot 
-Code written by Ben 
+Code written by Ben, adapted by Caroline 
 --------------------
 
 ```{r}
-pcaPlot<-function(ps, # clr transformed and filtered data
-                  colorVar, # variable from samdf to color samples by
-                  colorName, # what to display variable name as in legend
-                  nTaxa # number of taxa to display
-                  ) { 
-  if("name" %in% colnames(data.frame(ps@sam_data) )) { # there will be an error if there is a column called "name" in the ps object
-  sample_data(ps)=ps@sam_data%>%
-    data.frame() %>%
-    dplyr::rename(name.x=name)
+pcaPlot <- function(ps, # clr transformed and filtered data
+                    colorVar = NULL, # variable from samdf to color samples by
+                    colorName = NULL, # what to display variable name as in legend
+                    nTaxa = 10, # number of taxa to display
+                    customColors = NULL, # optional named vector of colors
+                    xPC = 1, # Principal Component for x-axis
+                    yPC = 2,  # Principal Component for y-axis
+                    ellipse = FALSE # optional add centroid ellipses 
+                    ) { 
+  
+  if ("name" %in% colnames(data.frame(ps@sam_data))) { 
+    # Prevent conflict with 'name' column
+    sample_data(ps) <- ps@sam_data %>%
+      data.frame() %>%
+      dplyr::rename(name.x = name)
   }
+  
   samdf <- data.frame(ps@sam_data) %>%
     rownames_to_column(var = 'name')
   
   # PCA
   pca <- prcomp(ps@otu_table, center = TRUE, scale = FALSE)
   
-  pca.df <- data.frame(pca$x) %>% 
-          rownames_to_column(var = 'name')
-  
   # % variance explained
   eigs <- pca$sdev^2
-  varExplained <- 100 * round(eigs/sum(eigs), 5)
+  varExplained <- 100 * eigs / sum(eigs)
+  names(varExplained) <- paste0('PC', seq_along(varExplained))
   
-  # pull out first 2 PC variance explained for the plot
-  ve.pc1 <- as.character(round(varExplained[1], 3))
-  ve.pc2 <- as.character(round(varExplained[2], 3))
-
+  # Extract variance explained for specified PCs
+  ve.xPC <- as.character(round(varExplained[paste0('PC', xPC)], 3))
+  ve.yPC <- as.character(round(varExplained[paste0('PC', yPC)], 3))
+  
+  # PCA scores
+  pca.df <- data.frame(pca$x) %>% 
+    rownames_to_column(var = 'name')
   
   # Add back sample data
-  pca.df <- left_join(pca.df, samdf)
-
-  # Calculate plotting limits based on largest value observed in PC axes 1 and 2
-  limit <- max(abs(pca.df[, c('PC1', 'PC2')])) +
-            0.05*(max(abs(pca.df[, c('PC1', 'PC2')])))
+  pca.df <- left_join(pca.df, samdf, by = "name")
   
+  # Calculate plotting limits based on specified PCs
+  limit <- max(abs(pca.df[, c(paste0('PC', xPC), paste0('PC', yPC))])) +
+    0.05 * max(abs(pca.df[, c(paste0('PC', xPC), paste0('PC', yPC))]))
   
-
-  pca.plot <- 
-       ggplot(pca.df, aes_string(x = "PC1", y = "PC2", color = colorVar)) +
-       geom_point(size = 2, alpha = 0.5) +
-       stat_ellipse(level = 0.95, aes_string(group = colorVar), linetype = "dashed") + # Add ellipses here
-       coord_equal() +
-       labs(x = paste0(' PC1 (', ve.pc1, '%)'),
-            y = paste0(' PC2 (', ve.pc2, '%)')) + 
-       xlim(-limit, limit) + ylim(-limit, limit)+
-       theme_classic() +
-       theme(axis.line = element_line(size = 1, color = 'black'),
-             axis.ticks = element_line(color = 'black'),
-             axis.title = element_text(size = 14, face = 'bold', color = 'black'),
-             # axis.text = element_blank(),
-             # legend.background = element_blank(),
-             # legend.title = element_blank(),
-             #legend.position = "none"
-             # legend.text = element_text(size = 10, face = 'bold'),
-             ) 
-        # scale_color_manual(values=met.brewer('Signac',10)) ### can change color palette here 
-
- pca.plot
-  # Biplot
+  # Initialize PCA plot
+  pca.plot <- ggplot(pca.df, aes_string(x = paste0('PC', xPC), y = paste0('PC', yPC), color = colorVar)) +
+    geom_point(size = 2, alpha = 0.5) +
+    coord_equal() +
+    labs(x = paste0('PC', xPC, ' (', ve.xPC, '%)'),
+         y = paste0('PC', yPC, ' (', ve.yPC, '%)')) + 
+    xlim(-limit, limit) + ylim(-limit, limit) +
+    theme_classic() +
+    theme(axis.line = element_line(size = 1, color = 'black'),
+          axis.ticks = element_line(color = 'black'),
+          axis.title = element_text(size = 14, face = 'bold', color = 'black'))
+  
+  # Add custom color scale if provided
+  if (!is.null(customColors)) {
+    pca.plot <- pca.plot + scale_color_manual(values = customColors)
+  }
+  
+  # Add optional ellipses
+  if (ellipse) {
+    pca.plot <- pca.plot + stat_ellipse(level = 0.95, aes_string(group = colorVar), linetype = "dashed")
+  }
   
   # Calculate loadings
   V <- pca$rotation # Eigenvectors
-  L <- diag(pca$sdev) # Diag mtx w/sqrts of eigenvalues on diag.
+  L <- diag(pca$sdev) # Diagonal matrix with square roots of eigenvalues
   loadings <- V %*% L
-       
-  # Get loadings for first 2 PCs and format for plotting
-  pythag <- function(a, b){sqrt(a^2 + b^2)}
-  loadings.12 <- data.frame(loadings[, 1:2]) %>%
-       dplyr::rename(PC1 = X1, PC2 = X2) %>% 
-       mutate(variable = row.names(loadings)) %>% 
-       mutate(length = pythag(PC1, PC2), slope = PC2/PC1, ang = atan(slope)*(180/pi))
+  colnames(loadings) <- colnames(V)  # Assign column names to loadings
   
-  loadings.plot <- top_n(loadings.12, nTaxa, wt = length) 
+  # Get loadings for specified PCs and format for plotting
+  loadings.xy <- data.frame(loadings[, c(paste0('PC', xPC), paste0('PC', yPC))]) %>%
+    dplyr::rename(PCx = paste0('PC', xPC), PCy = paste0('PC', yPC)) %>% 
+    mutate(variable = row.names(loadings),
+           length = sqrt(PCx^2 + PCy^2),
+           ang = atan2(PCy, PCx) * (180 / pi))
   
-   loadings.plot <- top_n(loadings.12, nTaxa, wt = length) 
+  loadings.plot <- top_n(loadings.xy, nTaxa, wt = length) 
+  
+  # Adjust angles to keep labels upright
+  loadings.plot <- loadings.plot %>%
+    mutate(adj_ang = ifelse(ang < -90, ang + 180,
+                     ifelse(ang > 90, ang - 180, ang)))
   
   # Rename loadings with lowest taxonomic level
   loadings.taxtab <- tax_table(ps)[row.names(loadings.plot)] %>% 
-       data.frame() 
+    data.frame() 
   loadings.taxtab <- loadings.taxtab[cbind(1:nrow(loadings.taxtab), max.col(!is.na(loadings.taxtab), ties.method = 'last'))] %>%  
     data.frame()
   colnames(loadings.taxtab) <- c("name")
@@ -90,58 +99,55 @@ pcaPlot<-function(ps, # clr transformed and filtered data
     data.frame() %>% 
     rownames()
   
-  
   loadings.plot <- loadings.taxtab %>% 
-       dplyr::select(asv,name) %>% 
-       right_join(loadings.plot, by = c('asv' = 'variable'))
+    dplyr::select(asv, name) %>% 
+    right_join(loadings.plot, by = c('asv' = 'variable'))
   
-  # What quadrant of the plot is the label in?
-  q1 <- filter(loadings.plot, PC1 > 0 & PC2 > 0)
-  q2 <- filter(loadings.plot, PC1 < 0 & PC2 > 0)
-  q3 <- filter(loadings.plot, PC1 < 0 & PC2 < 0)
-  q4 <- filter(loadings.plot, PC1 > 0 & PC2 < 0)
+  # Determine the quadrant of each label
+  q1 <- filter(loadings.plot, PCx > 0 & PCy > 0)
+  q2 <- filter(loadings.plot, PCx < 0 & PCy > 0)
+  q3 <- filter(loadings.plot, PCx < 0 & PCy < 0)
+  q4 <- filter(loadings.plot, PCx > 0 & PCy < 0)
        
   pca.biplot <- 
        pca.plot + 
        geom_segment(data = loadings.plot,
                     aes(x = 0, y = 0, 
-                        xend = PC1, yend = PC2),
+                        xend = PCx, yend = PCy),
                     color = 'black',
                     arrow = arrow(angle = 15, 
                                   length = unit(0.1, 'inches'))) + 
     labs(color = colorName)
   
-  # Then add geom_text quadrant-by-quadrant, aligning text accordingly
-       if (dim(q1)[1] != 0) {
-            pca.biplot <- pca.biplot +
-                 geom_text(data = q1, aes(x = PC1, y = PC2, hjust = 0, angle = ang,
-                                          label=paste0('   ', name),
-                                          fontface = 'bold'),
-                           color = 'black', show.legend = FALSE)
-       }
-       if (dim(q2)[1] != 0) {
-            pca.biplot <- pca.biplot +
-                 geom_text(data = q2, aes(x = PC1, y = PC2, hjust = 1, angle = ang,
-                                          label=paste0(name, '   '),
-                                          fontface = 'bold'),
-                           color = 'black', show.legend = FALSE)
-       }
-       if (dim(q3)[1] != 0) {
-            pca.biplot <- pca.biplot +
-                 geom_text(data = q3, aes(x = PC1, y = PC2, hjust = 1, angle = ang,
-                                          label=paste0(name, '   '),
-                                          fontface = 'bold'),
-                           color = 'black', show.legend = FALSE)
-       }
-       if (dim(q4)[1] != 0) {
-            pca.biplot <- pca.biplot +
-                 geom_text(data = q4, aes(x = PC1, y = PC2, hjust = 0, angle = ang,
-                                          label=paste0('   ', name),
-                                          fontface = 'bold'),
-                           color = 'black', show.legend = FALSE)
-       }
-  
-  #print(pca.biplot)
+  # Add geom_text for each quadrant with adjusted angle and justification
+  if (nrow(q1) != 0) {
+       pca.biplot <- pca.biplot +
+            geom_text(data = q1, aes(x = PCx, y = PCy, hjust = 0, vjust = 0, angle = adj_ang,
+                                     label = name,
+                                     fontface = 'bold'),
+                      color = 'black', show.legend = FALSE)
+  }
+  if (nrow(q2) != 0) {
+       pca.biplot <- pca.biplot +
+            geom_text(data = q2, aes(x = PCx, y = PCy, hjust = 1, vjust = 0, angle = adj_ang,
+                                     label = name,
+                                     fontface = 'bold'),
+                      color = 'black', show.legend = FALSE)
+  }
+  if (nrow(q3) != 0) {
+       pca.biplot <- pca.biplot +
+            geom_text(data = q3, aes(x = PCx, y = PCy, hjust = 1, vjust = 1, angle = adj_ang,
+                                     label = name,
+                                     fontface = 'bold'),
+                      color = 'black', show.legend = FALSE)
+  }
+  if (nrow(q4) != 0) {
+       pca.biplot <- pca.biplot +
+            geom_text(data = q4, aes(x = PCx, y = PCy, hjust = 0, vjust = 1, angle = adj_ang,
+                                     label = name,
+                                     fontface = 'bold'),
+                      color = 'black', show.legend = FALSE)
+  }
   
   return(list(pca.df = pca.df, pca.biplot = pca.biplot, loadings = loadings))
 }
