c163a55bd9dccb03c0d0b216175c94cb6bc4077f
cr403 <151646120+cr403@users.noreply.github.com>
2025-03-10 13:16:20 -0400

    Create remNAcol.R
    Function that removes sample data columns with all NA's in a phyloseq object. Useful for cleaning data, especially when sampling from mega-phyloseq object.

---
+++ R/remNAcol.R
@@ -0,0 +1,20 @@
+remNAcol <- function(ps # phyloseq object
+                     ) {
+  object_name <- deparse(substitute(ps)) # Captures the name of the phyloseq object 
+  
+  before_removal <- dim(ps@sam_data) 
+  
+  df <- ps@sam_data %>%
+    data.frame() %>%
+    select(where(~ !all(is.na(.))))
+  
+  ps@sam_data <- sample_data(df)
+  
+  after_removal <- dim(ps@sam_data) 
+  
+  print(paste0("Dimensions of ", object_name, "@sam_data ", "before removal: ", before_removal[1], " ", before_removal[2])) 
+  print(paste0("Dimensions of ", object_name,"@sam_data ", "after removal: ", after_removal[1], " ", after_removal[2])) 
+  print(paste0("Columns removed: ", before_removal[2] - after_removal[2]))
+  
+  return(ps)
+}
