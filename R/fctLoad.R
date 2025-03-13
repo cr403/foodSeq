7ed500a5225190cae7e04d885428a0141e20a1c0
cr403 <151646120+cr403@users.noreply.github.com>
2025-03-13 16:21:22 -0400

    Create fctLoad.R
    Takes in pca_output from pcaPlot.R (needs factor loadings data frame), phyloseq object with tax_table that has CommonName, the desired principal component (default to 1), and the number of taxa for graphing (default to 10). Returns data frame with asv, CommonName, and all PC's with the PC of interest at the front. Also returns horizontal bar chart of top nTaxa loadings.

---
+++ R/fctLoad.R
@@ -0,0 +1,39 @@
+library(phyloseq) 
+library(tidyverse) 
+
+fctLoad <- function(physeq, 
+                    pca_output, 
+                    PCx = 1,
+                    nTaxa = 10){
+  
+  # Extract taxa information 
+  taxtab <- ps.plot@tax_table %>%
+    data.frame() %>%
+    rownames_to_column(var = "asv")
+  
+  # Extract loadings 
+  load.df <- pca_output$loadings %>% 
+    data.frame() %>%
+    rownames_to_column(var = "asv") %>%
+    left_join(taxtab, by = "asv") 
+  
+  # Order loadings by absolute value 
+  PC_col <- sym(paste0("PC", PCx)) 
+  
+  result <- load.df %>% 
+    slice_max(order_by = abs(-!!PC_col), n = nTaxa) %>%
+    relocate(CommonName, .after = asv) %>%
+    relocate(PC_col, .after = CommonName)  
+  
+  # Plot top nTaxa 
+  plot <- result %>% 
+    ggplot(aes(x = fct_reorder(CommonName, !!PC_col), y = !!PC_col)) + 
+    geom_bar(stat = "identity") + 
+    coord_flip() + 
+    theme(axis.title = element_text(size = 16, face = "bold"), 
+        axis.title.y = element_blank(),
+        axis.text = element_text(size = 14))
+  
+  return(list(load.df = result, plot = plot)) 
+  
+}
