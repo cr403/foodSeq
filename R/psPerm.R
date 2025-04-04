fe8a5d25fee4c6c6a799f779ae79b5c533359526
cr403 <151646120+cr403@users.noreply.github.com>
2025-04-04 15:55:11 -0400

    Update psPerm.R
    Added extra description of the use of the "by" parameter

--- R/psPerm.R
+++ R/psPerm.R
@@ -2,6 +2,18 @@ library(vegan)
 library(phyloseq)
 library(tidyverse) 
 
+# Note - older versions of adonis2 (pre-August 2024) have `by="terms"` as default, but to have things separated out by term in 
+# the new version, this must be input manually. 
+# 
+# Note - `by = "terms"` used for a sequential test of model terms where the order of terms can matter. For example `~ country + age`, 
+# you could interpret as age explains x% of variation when controlled for country. `~ age + country` can be interpreted as country explains 
+# x% variation when controlling for age. This is because PERMANOVA uses sequential sums of squares. So make sure to use the right order 
+# depending on the question you're asking. 
+#
+# The other option is to use `by = "margin"` where the order of terms does not matter. For example the output of `~ country + age` could be 
+# interpreted as age explains x% of variation when controlled for country while country explains y% of variance when controlled for age. In
+# other words, using `by = "margin"`, predictors are tested while controlling for all other variables in the equation. 
+
 psPerm <- function(physeq, 
                    equation, # e.g., "country + age + treatment" or "country*age + bmi"
                    method = "euclidean", 
