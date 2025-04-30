f61dfc1e6be752cb37ff852b016147a9429ec6a7
Caroline Rivera <caroline.rivera@duke.edu>
2025-04-30 14:43:36 -0400

    Initializing new function to wrap long strings at specified character length breakpoints. Used for visual purposes in graphing

---
+++ R/wrapLabels.R
@@ -0,0 +1,19 @@
+#' @title Wrap Labels
+#'
+#' @description Takes in a string and adds line breaks at specified breakpoints
+#'
+#' @param x string of interest
+#' @param width character length for breakpoint
+#'
+#' @return input string with new line at every width characters
+#'
+#' @export
+wrapLabels <- function(x, width = 20) {
+  sapply(x, function(str) {
+    if (nchar(str) > width) {
+      paste(strwrap(str, width = width), collapse = "\n")
+    } else {
+      str
+    }
+  }, USE.NAMES = FALSE)
+}
