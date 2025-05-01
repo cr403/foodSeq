bd91000e5e5525dfb48698df054125c4237cc7a1
Caroline Rivera <caroline.rivera@duke.edu>
2025-05-01 10:57:56 -0400

    made common name column name flexible, added label wrapping feature, and changed labeling to default to common name but pull in lowestLevel in the event of a missing common name

--- R/fctLoad.R
+++ R/fctLoad.R
@@ -6,6 +6,8 @@
 #' @param pca_output output object from pcaPlot()
 #' @param PCx principal component for x-axis of graph
 #' @param nTaxa number of taxa to include on plot
+#' @param name name of common name column
+#' @param labWidth character length breakpoint for wrapping labels
 #'
 #' @return load.df = loadings data frame in descending order by PCx
 #' @return plot = plot of top nTaxa taxa for PCx
@@ -14,7 +16,9 @@
 fctLoad <- function(physeq,
                     pca_output,
                     PCx = 1,
-                    nTaxa = 10){
+                    nTaxa = 10,
+                    name = "ShortName",
+                    labWidth = 60){
 
   # Extract taxa information
   taxtab <- physeq@tax_table %>%
@@ -26,20 +30,21 @@ fctLoad <- function(physeq,
   load.df <- pca_output$loadings %>%
     data.frame() %>%
     rownames_to_column(var = "asv") %>%
-    left_join(taxtab, by = "asv")
+    left_join(taxtab, by = "asv") %>%
+    mutate(label = ifelse(is.na(.data[[name]]), lowestLevel, .data[[name]]),
+           label = wrapLabels(label, labWidth))
 
   # Order loadings by absolute value
   PC_col <- sym(paste0("PC", PCx))
 
   result <- load.df %>%
-    filter(!is.na(ShortName)) %>%
     slice_max(order_by = abs(-!!PC_col), n = nTaxa) %>%
-    relocate(ShortName, .after = asv) %>%
-    relocate(PC_col, .after = ShortName)
+    relocate(label, .after = asv) %>%
+    relocate(PC_col, .after = label)
 
   # Plot top nTaxa
   plot <- result %>%
-    ggplot(aes(x = fct_reorder(ShortName, !!PC_col), y = !!PC_col)) +
+    ggplot(aes(x = fct_reorder(label, !!PC_col), y = !!PC_col)) +
     geom_bar(stat = "identity") +
     coord_flip() +
     theme(axis.title = element_text(size = 16, face = "bold"),
