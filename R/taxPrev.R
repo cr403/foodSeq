6240527ba807cf9818f1557a1377118a58c122c7
Caroline Rivera <caroline.rivera@duke.edu>
2025-05-02 16:07:22 -0400

    fixed bug where functions without facet weren't working. also added comments everywhere

--- R/taxPrev.R
+++ R/taxPrev.R
@@ -6,6 +6,7 @@
 #' @param taxList list of asv's of interest
 #' @param name name of common name column in tax table of phyloseq object
 #' @param facet name of samdf column by which to facet (optional)
+#' @param stripText optional labels for strip text
 #' @param nrow number of rows for facet_wrap()
 #' @param title optional title of plot
 #' @param taxListTitle optional title for taxList (y axis title)
@@ -36,17 +37,20 @@ taxPrev <- function(physeq, # phyloseq object
 ){
   ps <- physeq
 
+  # Extract tax table
   taxdf <- ps@tax_table %>%
     data.frame() %>%
     rownames_to_column(var = "asv") %>%
     filter(asv %in% taxList) %>%
-    mutate(label = ifelse(is.na(.data[[name]]), lowestLevel, .data[[name]]),
+    mutate(label = ifelse(is.na(.data[[name]]), lowestLevel, .data[[name]]), # build label for plotting
            label = wrapLabels(label, labWidth))
 
+  # Extract sample data
   samdf <- ps@sam_data %>%
     data.frame() %>%
     rownames_to_column(var = "samid")
 
+  # Take out facet variable from sample data
   if(!is.null(facet)) {
     samdf <- samdf %>%
       select(samid, .data[[facet]])
@@ -55,6 +59,7 @@ taxPrev <- function(physeq, # phyloseq object
       select(samid)
   }
 
+  # Extract otu table and join with tax/samdf tables
   seqdf <- ps@otu_table %>%
     data.frame() %>%
     rownames_to_column(var = "samid") %>%
@@ -62,51 +67,83 @@ taxPrev <- function(physeq, # phyloseq object
     filter(asv %in% taxList) %>%
     left_join(samdf, by = "samid") %>%
     left_join(taxdf, by = "asv") %>%
-    mutate(presence = ifelse(value > 0, 1, 0))
+    mutate(presence = ifelse(value > 0, 1, 0)) # Convert to presence/absence
 
-  taxList.prev <- seqdf %>%
-    group_by(.data[[facet]], label) %>%
-    summarise(prevalence = sum(presence) / n_distinct(samid) * 100) %>%
-    ungroup()
 
+  # Build prevalence data frame within facet
   if (!is.null(facet)) {
+    taxList.prev <- seqdf %>%
+      group_by(.data[[facet]], label) %>%
+      summarise(prevalence = sum(presence) / n_distinct(samid) * 100) %>%
+      ungroup()
+
     prev.plot <- taxList.prev %>%
       ggplot(aes(x = tidytext::reorder_within(label, by = prevalence, within = .data[[facet]]), y = prevalence)) +
-      facet_wrap(~.data[[facet]], scales = "free_y", nrow = nrow) +
       tidytext::scale_x_reordered()
+
+    # Add optional strip text labels
+    if (!is.null(stripText)) {
+      prev.plot <- prev.plot %>%
+        facet_wrap(~.data[[facet]], scales = "free_y", nrow = nrow, labeller = labeller(.data[[facet]] = stripText))
+    } else {
+      taxList.prev <- seqdf %>%
+        group_by(label) %>%
+        summarise(prevalence = sum(presence) / n_distinct(samid) * 100) %>%
+        ungroup()
+
+      prev.plot <- prev.plot
+        facet_wrap(~.data[[facet]], scales = "free_y", nrow = nrow)
+      }
+
+  # Build prevalence data frame within entire phyloseq
   } else {
+    taxList.prev <- seqdf %>%
+      group_by(label) %>%
+      summarise(prevlaence = sum(presence) / n_distinct(samid) * 100) %>%
+      ungroup()
+
     prev.plot <- taxList.prev %>%
       ggplot(aes(x = fct_reorder(label, prevalence), y = prevalence))
   }
 
+  # Add bars with color to graph
   if (color) {
+
+    # Local coloring
     if (localCol) {
       prev.plot <- prev.plot + geom_bar(stat = "identity", aes(fill = tidytext::reorder_within(label, by = prevalence, within = .data[[facet]]))) +
         theme(legend.position = "none")
+
+    # Global coloring
     } else {
       prev.plot <- prev.plot + geom_bar(stat = "identity", aes(fill = label)) +
         theme(legend.position = "none")
     }
 
+  # Add bars without color to graph
   } else {
     prev.plot <- prev.plot + geom_bar(stat = "identity")
   }
 
+  # Build remaining parts of graph
   prev.plot <- prev.plot +
     coord_flip() +
     labs(y = "Prevalence (% Samples w/ Taxa)") +
     theme(title = element_text(size = titleSize, face = "bold"),
           axis.text = element_text(size = textSize))
 
+  # Set axis limits
   if(ylim100) {
     prev.plot <- prev.plot +
       ylim(0,100)
   }
 
+  # Add title (default is no title)
   if(!is.null(title)) {
     prev.plot <- prev.plot + labs(title = title)
   }
 
+  # Add axis title (default is no title)
   if(!is.null(taxListTitle)) {
     prev.plot <- prev.plot + labs(x = taxListTitle)
   } else {
