a89413886b06eac30d54ef1be30fe4c9d35de2ec
Caroline Rivera <caroline.rivera@duke.edu>
2025-05-15 16:18:45 -0400

    updating documentation and also added grouping customization to naCheck

--- R/naCheck.R
+++ R/naCheck.R
@@ -1,23 +1,41 @@
 #' @title NA Checker
 #'
-#' @description Checks a phyloseq object for any taxa that have no superkingdom assignment
+#' @description Checks a phyloseq object for any taxa that have no superkingdom/kingdom assignment
 #'
 #' @param physeq phyloseq object of interest
+#' @param trnl trnl or 12sv5
+#' @param group grouping variable (e.g., study, location, etc.)
 #'
 #' @return for each study in the phyloseq object, returns the number of NA's present
 #'
 #' @export
-naCheck <- function(physeq){
-  studies <- physeq@sam_data %>% data.frame() %>% pull(study) %>% unique()
+naCheck <- function(physeq,
+                    trnl = TRUE,
+                    group = NULL){
 
-  for(i in studies) {
+  if (!is.null(group)) {
+    groups <- physeq@sam_data %>%
+      data.frame() %>%
+      pull(.data[[group]]) %>%
+      unique()
+  } else {
+    groups <- 1
+  }
+
+  if (trnl) {
+    taxlevel <- "superkingdom"
+  } else {
+    taxlevel <- "kingdom"
+  }
+
+  for(i in groups) {
 
     df <- physeq %>%
-      prune_samples(get_variable(physeq, "study") %in% i, .) %>%
+      prune_samples(get_variable(physeq, .data[[group]]) %in% i, .) %>%
       prune_taxa(taxa_sums(.) > 0, .) %>%
       tax_table() %>%
-      data.frame() %>%
-      filter(is.na(superkingdom))
+      data.frame()
+      filter(is.na(.data[[taxlevel]]))
 
     cat(i, " has ", nrow(df), " NA's", "\n")
   }
