aadac8356737b873da67ead24cb8a3822222751f
Caroline Rivera <caroline.rivera@duke.edu>
2025-05-27 15:40:38 -0400

    fixed parentheses typo

--- R/top_n_taxa.R
+++ R/top_n_taxa.R
@@ -73,7 +73,7 @@ top_n_taxa <- function(physeq,
   # Add taxonomy labels
   top_taxa <- taxtab %>%
     left_join(seqtab, by = "asv") %>%
-    mutate(label = ifelse(is.na(.data[[name]], lowestLevel, .data[[name]])),
+    mutate(label = ifelse(is.na(.data[[name]]), lowestLevel, .data[[name]]),
            label = case_when(
              is.na(label) ~ paste0("NA", row_number()),
              TRUE ~ label
